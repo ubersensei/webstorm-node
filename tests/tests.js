a7dabddb5acb742d297873cb43a98292002e0951
Kaya <ubersensei@gmail.com>
2015-04-19 23:02:51 +0800

    second commit

---
+++ tests/tests.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sajo5ua on 19/4/15.
+ * Description: ridiculous tests
+ */
+
+
+var should = require('should');
+var assert = require('assert');
+
+describe('Test Framework', function () {
+    it('should have mocha installed and running', function () {
+        assert.equal(true, true);
+    });
+    it('should have the should library installed and running for fluent testing', function () {
+        true.should.eql(true);
+    })
+});
